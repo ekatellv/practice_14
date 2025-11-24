e967360740fb6e998d98ee85fa79eeccfaf81730
ekatellv <159680448+ekatellv@users.noreply.github.com>
2025-11-24 18:42:04 +0700

    Create ex_10.py

---
+++ ex_10.py
@@ -0,0 +1,20 @@
+text_1 = input('enter first integers: ').split()
+text_2 = input('enter second integers: ').split()
+begin_range = int(input('enter begin range: '))
+end_range = int(input('enter end range: '))
+
+lst_1 = [int(x) for x in text_1]
+lst_2 = [int(x) for x in text_2]
+
+# Основываясь на примере из условия
+start_ind = begin_range - 1
+end_ind = end_range + 1
+
+elements = lst_1[start_ind:end_ind]
+elements.reverse()
+
+lst_2.extend(elements)
+del lst_1[start_ind:end_ind]
+
+print("The first list: ", lst_1)
+print("The second list: ", lst_2)
