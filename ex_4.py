5843c7ee734382158b3b787ae918e051e5e75801
ekatellv <159680448+ekatellv@users.noreply.github.com>
2025-11-25 08:46:41 +0700

    Update ex_4.py

--- ex_4.py
+++ ex_4.py
@@ -1,5 +1,8 @@
 import re
 
 text = input('enter a sentences: ')
+
 words = re.findall(r'[a-zA-Zа-яА-ЯёЁ]+', text)
-print(list(dict.fromkeys(words)))
+words_lower = [word.lower() for word in words] 
+
+print(list(dict.fromkeys(words_lower)))
