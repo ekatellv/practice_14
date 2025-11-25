de481e89c6bd16e56beb3e8b9fd8ede91f116081
ekatellv <159680448+ekatellv@users.noreply.github.com>
2025-11-25 08:38:35 +0700

    Update ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -2,3 +2,12 @@ text = input('enter integers in one line, including 3: ')
 parts = text.split()
 result = [int(x) for x in parts if x != '3']
 print(result)
+
+# Альтернатива
+numbers = list(map(int, input('enter integers in one line, including 3: '").split()))
+new_list = numbers.copy()
+
+if 3 in new_list:
+    new_list.remove(3)
+
+print(new_list)
